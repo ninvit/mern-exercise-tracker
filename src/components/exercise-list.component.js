8df70b80cf2127854c5cf43be3cd35810efd9184
ninvit <ninvit@gmail.com>
2020-03-30 13:08:47 -0300

    form-exercise added

---
+++ src/components/exercise-list.component.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+export default class ExerciseList extends Component {
+    render() {
+        return(
+            <div>
+                <p>Exercises List Component!</p>
+            </div>
+        );
+    }
+}
